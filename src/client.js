7b29e1ad0415007c1247eec36b31a7855af787de
Shane Garrity <shanedgarrity@gmail.com>
2018-08-06 01:10:26 -0400

    remove a lot, strip back to basics

--- src/client.js
+++ src/client.js
@@ -1,3 +1,6 @@
+// TODO: actually import styles here
+// and I guess html, etc files as well??
+
 (function () {
   
   /*
@@ -6,25 +9,6 @@
   var init = function init() {
     document.addEventListener('DOMContentLoaded', function() {
       noJS();
-      setTimeout(function() {
-        document.querySelector('.page').className += ' fly-in';
-      }, 100);
-
-      document.querySelector('.open').addEventListener('click', function(e) {
-        e.preventDefault();
-        var more = document.querySelector('.more-wrap');
-        if (more.className.indexOf(' active') === -1) {
-          more.className += ' active';
-        } else {
-          more.className = more.className.replace(' active', '');
-          menuScroll();
-        }
-      });
-      
-      window.addEventListener('scroll', (e) => {
-        debounce(menuScroll, 5);
-      });
-
     });
   };
 
@@ -46,30 +30,6 @@
     } else {
       window.clearTimeout(window.debounceGlobal);
       window.debounceGlobal = null;
-    }
-  };
-
-  /*
-   *
-   */
-  var menuScroll = function menuScroll() {
-    var windowScroll = window.scrollY;
-    var el = document.querySelector('.responsibilities');
-    var elScroll = el.offsetTop - 50;
-    var openEl = document.querySelector('.offscreen');
-
-    if (windowScroll > elScroll) {
-      console.log('windowScroll > elScroll');
-      if (openEl.className.indexOf(' min') === -1) {
-        var more = document.querySelector('.more-wrap');
-        more.className = more.className.replace(' active', '');
-        openEl.className += ' min';
-      }
-    } else {
-      console.log('windowScroll <= elScroll');
-      if (openEl.className.indexOf(' min') > -1) {
-        openEl.className = openEl.className.replace(' min', '');
-      }
     }
   };
 
